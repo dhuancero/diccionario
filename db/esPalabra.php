bc152ea704acfcb4f5384a26398d37ba475cab19
DHA DESARROLLOS <dha.desarrollos@gmail.com>
2022-08-04 10:55:41 +0200

    insert Into de relacion  y palabra

--- db/esPalabra.php
+++ db/esPalabra.php
@@ -12,62 +12,93 @@ if (isset($_POST["registrar"])) {
   $descripcion = $_POST["descripcion"];
   $numPersonajes = $_POST['val-person'];
   $nombreImagen = addslashes($_FILES["imagen"]["name"]);
-  echo $nombreImagen;
+  echo $nombreImagen . "<br>";
 
   //! Comprobamos que los campos tienen contenido
   if ($palabra === "" || $descripcion === "" || $numPersonajes === "") {
     exit(header("Location: ../pages/insertar-palabra.php?msj=errorReg"));
   }
 
+  // TODO: Comprobar si la palabra ya ha sido intprducida. 
+  // TODO: si ya ha sido introducida->msj:error_palabra-> no deja guardar palabra
+
+  //TODO: Pendiente de realizar la inserción
+
   //! Comprobamos que la imagen ha sido cargada
   $nombreArchivo = ($nombreImagen !== "") ? $nombreImagen : "avatar_1.jpg";
   $tmpImagen = $_FILES['imagen']['tmp_name'];
   echo $tmpImagen . "<br>";
+  $ruta = $_SERVER["DOCUMENT_ROOT"];
+  echo "<br>" . $_SERVER["PHP_SELF"] . "<br>";
+  echo "<br>" . $_SERVER["DOCUMENT_ROOT"] . "<br>";
+
+  if (is_uploaded_file($tmpImagen)) {
+    echo "si se ha guardado";
+    move_uploaded_file(addslashes($tmpImagen), addslashes("img/" . $nombreArchivo));
+    // copy(addslashes($tmpImagen), addslashes($ruta . "/diccionario/img/" . $nombreArchivo));
+  } else {
+    echo "NO se ha subido";
+  }
   if ($tmpImagen !== "") {
-    move_uploaded_file(addslashes($tmpImagen), "../img/" . $nombreArchivo);
+    move_uploaded_file(addslashes($tmpImagen), addslashes($ruta . "/diccionario/img/" . $nombreArchivo));
   }
 
-  //! PRIMERA CONSULTA: INSERTAR PALABRA.
-  $sql_palabra = "INSERT INTO palabras(palabra, imagen, descripcion) VALUES (?,?,?)";
+  //?----------------------------------------------------------------------
+  //? PRIMERA CONSULTA: INSERTAR PALABRA e IMAGEN.
+  //?----------------------------------------------------------------------
 
-  // $sql = "INSERT INTO `usuarios`(`nombre`, `email`, `username`, `password`, `re-password`) VALUES (?,?,?,?,?)";
-  $sentencia = $conn->prepare($sql_palabra);
-  $sentencia->bind_param('sss', $palabra, $nombreImagen, $descripcion);
-
-  $sentencia->execute();
-  if ($sentencia) {
+  $sql_palabra = "INSERT INTO palabras (palabra, imagen, descripcion) VALUES (?,?,?)";
+  $insertarPalabra = $conn->prepare($sql_palabra);
+  $insertarPalabra->bind_param('sss', $palabra, $nombreArchivo, $descripcion);
+  $insertarPalabra->execute();
+  if ($insertarPalabra) {
     echo "Todo ha salido bien";
     // header("Location: ../pages/insertar-palabra.php?msj=ok");
   } else {
     echo "No se ha podido insertar registro";
   }
+  $insertarPalabra->close();
 
-  $stmt->close();
-  $db->close();
+  //?----------------------------------------------------------------------
+  //? SEGUNDA CONSULTA: Recuperar valores para(id) para palabra y personaje
+  //?----------------------------------------------------------------------
 
-  //! SEGUNDA CONSULTA: INSERTAR RELACIÓN.
+  //!recupero la ID de la palabra introducida:
+  //!----------------------------------------------------------------------
 
-  $sql_select_personaje = "SELECT `id`, `nombre` FROM `personaje` WHERE `id` =" . $numPersonajes;
+  $sql_nom_palabra = "SELECT id, palabra FROM palabras WHERE palabra = ?";
+  // $sentencia = $conn->query($sql_nom_palabra);
+  $sent_word = $conn->prepare($sql_nom_palabra);
+  $sent_word->bind_param("s", $palabra);
+  $sent_word->execute();
+  $resultado = $sent_word->get_result();
+  # Obtenemos solo una fila, que será la palabra a mostrar
+  $word = $resultado->fetch_assoc();
+  if (!$word) {
+    exit("No hay resultados para ese palabra");
+  }
+  $sent_word->close();
+  $valWord = $word['palabra'];
+  $idWord = $word['id'];
 
+  //! Recupero los ids de los personajes, dependiendo si han introducido uno o más personajes.
+  //!----------------------------------------------------------------------
 
-  $sql_personaje = "INSERT INTO `personaje`(`id`, `nombre`, `imagen`, `id_palabra`) VALUES ()";
-
-  $sql_relacion = "INSERT INTO `relacion`(`id_palabra`, `id_personaje`) VALUES ()";
-
-  echo "<br><hr><br>";
-
-  echo "Palabra: " . $palabra . "<br>";
-  echo "<br><hr><br>";
-
-  // echo "state: " . $state . "<br>";
-  echo "Descripción: " . $descripcion . "<br>";
-  echo "<br><hr><br>";
-
-  echo "Imangen: " . $imgContent . "<br>";
-  echo "<br><hr><br>";
-
-  echo "Personajes(por ID):" . $numPersonajes . "<br>";
-  echo "<br><hr><br>";
-
-  echo $_SERVER['DOCUMENT_ROOT'] . "<br>";
+  $arrayPersonaje = explode(",", $numPersonajes);
+  foreach ($arrayPersonaje as $idPersonaje) {
+    echo "ID Personaje: " . $idPersonaje . "<br>";
+    //? Creo la consulta de insersion por cada personaje y por
+    //?----------------------------------------------------------------------
+    $sql_relacion = "INSERT INTO relacion (id_palabra, id_personaje) VALUES (?,?)";
+    $inser_relacion = $conn->prepare($sql_relacion);
+    $inser_relacion->bind_param('ii', intval($idWord), intval($idPersonaje));
+    $inser_relacion->execute();
+    if ($inser_relacion) {
+      echo "Todo ha salido bien <br>";
+      // header("Location: ../pages/insertar-palabra.php?msj=ok");
+    } else {
+      echo "No se ha podido insertar registro";
+    }
+    $inser_relacion->close();
+  };
 }
